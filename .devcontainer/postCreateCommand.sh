361fa8578b621a0da29b99ec95bc9088807c6b20
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2025-10-17 20:05:33 +0900

    Update Dockerfile and configuration for cross-compilation support and dependencies

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rustup target add aarch64-unknown-linux-gnu
+rustup target add arm-unknown-linux-gnueabihf
 pip3 install -r .devcontainer/requirements.txt
 
 git clone https://github.com/ryuz/vitisenv.git ~/.vitisenv
