6379f13ec84bc3043f83f319548493d765bc82cf
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2025-10-21 20:34:14 +0900

    Update Docker images and refactor Makefiles for kv260 and zybo_z7 projects

--- projects/zybo_z7/zybo_z7_rtcl_p3s7_hs/app/rust/src/main.rs
+++ projects/zybo_z7/zybo_z7_rtcl_p3s7_hs/app/rust/src/main.rs
@@ -1,12 +1,37 @@
+
+use jelly_mem_access::*;
+
+
 use opencv::{
     core::*,
     highgui::*,
 };
 
 fn main() {
-    println!("Hello, world!");
+    println!("start zybo_z7_rtcl_p3s7_hs");
+
+    /*
     let img : Mat = Mat::zeros(480, 640, opencv::core::CV_8UC3).unwrap().to_mat().unwrap();
     println!("img = {:?}", img);
     imshow("test", &img).unwrap();
     wait_key(0).unwrap();
+    */
+
+
+    // mmap udmabuf
+    let udmabuf_device_name = "udmabuf-jelly-vram0";
+    println!("\nudmabuf open");
+    let udmabuf_acc =
+        UdmabufAccessor::<usize>::new(udmabuf_device_name, false).expect("Failed to open udmabuf");
+    println!(
+        "{} phys addr : 0x{:x}",
+        udmabuf_device_name,
+        udmabuf_acc.phys_addr()
+    );
+    println!(
+        "{} size      : 0x{:x}",
+        udmabuf_device_name,
+        udmabuf_acc.size()
+    );
+
 }
