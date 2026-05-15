f867b556a9c56ef10eeefcea9ef5a549056743e2
Ryuji Fuchikami (@Ryuz88) <ryuji.fuchikami@nifty.com>
2026-05-15 22:43:14 +0900

    Feature/kv260 rtcl hub75e sample (#31)
    * add kv260_rtcl_hub75e_sample
    
    * Update kv260_rtcl_hub75e_sample project with various enhancements and fixes
    
    - Updated submodule reference for jelly.
    - Corrected comment in device tree source (DTS) for M_AXI_HPM0_FPD.
    - Added debug print statements in Rust main function for VRAM write operations.
    - Changed data type for VRAM write function to u32 in Rust code.
    - Modified SystemVerilog parameters for HUB75E memory and AXI4 interface.
    - Removed unused commented code in SystemVerilog files.
    - Added ODDRE1 module stub for simulation.
    - Expanded testbench to include additional AXI4 signals and functionality.
    - Created Makefile for Verilator simulation setup.
    - Updated synthesis and implementation scripts for Vivado 2024.2, including new source files.
    
    * Enhance kv260_rtcl_hub75e_sample project with clock signal adjustments and module updates
    
    - Refactored clock signal handling in kv260_rtcl_hub75e_sample.sv to include sys_clk50 and sys_clk50_90.
    - Updated design_1.v to reflect changes in output clock signals, adding out_clk50_90.
    - Modified rtcl_hub75e_pmod.sv to replace clk_x2 with clk_90 and streamline signal assignments.
    - Adjusted tb_main.sv and tb_top.sv to incorporate new clock signals and ensure proper simulation behavior.
    - Updated design_1.tcl and kv260_rtcl_hub75e_sample.xpr to include new clock configurations and synthesis settings for improved performance.
    
    * Add README for kv260_rtcl_hub75e_sample project

---
+++ projects/kv260/kv260_rtcl_hub75e_sample/constrain/top.xdc
@@ -0,0 +1,46 @@
+set_property PACKAGE_PIN A12 [get_ports fan_en]
+set_property IOSTANDARD LVCMOS33 [get_ports fan_en]
+
+
+################################
+# Timing
+################################
+
+# clk_out1_design_1_clk_wiz_0_0       50MHz
+# clk_out2_design_1_clk_wiz_0_0      100MHz
+# clk_out3_design_1_clk_wiz_0_0      200MHz
+
+
+# PMOD
+set_property PACKAGE_PIN H12 [get_ports {pmod[0]}]
+set_property PACKAGE_PIN E10 [get_ports {pmod[1]}]
+set_property PACKAGE_PIN D10 [get_ports {pmod[2]}]
+set_property PACKAGE_PIN C11 [get_ports {pmod[3]}]
+set_property PACKAGE_PIN B10 [get_ports {pmod[4]}]
+set_property PACKAGE_PIN E12 [get_ports {pmod[5]}]
+set_property PACKAGE_PIN D11 [get_ports {pmod[6]}]
+set_property PACKAGE_PIN B11 [get_ports {pmod[7]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {pmod[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {pmod[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {pmod[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {pmod[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {pmod[4]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {pmod[5]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {pmod[6]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {pmod[7]}]
+set_property DRIVE 8 [get_ports {pmod[0]}]
+set_property DRIVE 8 [get_ports {pmod[1]}]
+set_property DRIVE 8 [get_ports {pmod[2]}]
+set_property DRIVE 8 [get_ports {pmod[3]}]
+set_property DRIVE 8 [get_ports {pmod[4]}]
+set_property DRIVE 8 [get_ports {pmod[5]}]
+set_property DRIVE 8 [get_ports {pmod[6]}]
+set_property DRIVE 8 [get_ports {pmod[7]}]
+set_property SLEW SLOW [get_ports {pmod[0]}]
+set_property SLEW SLOW [get_ports {pmod[1]}]
+set_property SLEW SLOW [get_ports {pmod[2]}]
+set_property SLEW SLOW [get_ports {pmod[3]}]
+set_property SLEW SLOW [get_ports {pmod[4]}]
+set_property SLEW SLOW [get_ports {pmod[5]}]
+set_property SLEW SLOW [get_ports {pmod[6]}]
+set_property SLEW SLOW [get_ports {pmod[7]}]
---
+++ projects/kv260/kv260_rtcl_hub75e_sample/sim/stub/design_1.v
@@ -0,0 +1,140 @@
+`timescale 1 ps / 1 ps
+
+module design_1
+   (fan_en,
+    m_axi4_aclk,
+    m_axi4_araddr,
+    m_axi4_arburst,
+    m_axi4_arcache,
+    m_axi4_aresetn,
+    m_axi4_arid,
+    m_axi4_arlen,
+    m_axi4_arlock,
+    m_axi4_arprot,
+    m_axi4_arqos,
+    m_axi4_arready,
+    m_axi4_arsize,
+    m_axi4_aruser,
+    m_axi4_arvalid,
+    m_axi4_awaddr,
+    m_axi4_awburst,
+    m_axi4_awcache,
+    m_axi4_awid,
+    m_axi4_awlen,
+    m_axi4_awlock,
+    m_axi4_awprot,
+    m_axi4_awqos,
+    m_axi4_awready,
+    m_axi4_awsize,
+    m_axi4_awuser,
+    m_axi4_awvalid,
+    m_axi4_bid,
+    m_axi4_bready,
+    m_axi4_bresp,
+    m_axi4_bvalid,
+    m_axi4_rdata,
+    m_axi4_rid,
+    m_axi4_rlast,
+    m_axi4_rready,
+    m_axi4_rresp,
+    m_axi4_rvalid,
+    m_axi4_wdata,
+    m_axi4_wlast,
+    m_axi4_wready,
+    m_axi4_wstrb,
+    m_axi4_wvalid,
+    m_axi4l_aclk,
+    m_axi4l_araddr,
+    m_axi4l_aresetn,
+    m_axi4l_arprot,
+    m_axi4l_arready,
+    m_axi4l_arvalid,
+    m_axi4l_awaddr,
+    m_axi4l_awprot,
+    m_axi4l_awready,
+    m_axi4l_awvalid,
+    m_axi4l_bready,
+    m_axi4l_bresp,
+    m_axi4l_bvalid,
+    m_axi4l_rdata,
+    m_axi4l_rready,
+    m_axi4l_rresp,
+    m_axi4l_rvalid,
+    m_axi4l_wdata,
+    m_axi4l_wready,
+    m_axi4l_wstrb,
+    m_axi4l_wvalid,
+    out_clk100,
+    out_clk50,
+    out_clk50_90,
+    out_reset);
+  output [0:0]fan_en;
+  output m_axi4_aclk;
+  output [39:0]m_axi4_araddr;
+  output [1:0]m_axi4_arburst;
+  output [3:0]m_axi4_arcache;
+  output [0:0]m_axi4_aresetn;
+  output [15:0]m_axi4_arid;
+  output [7:0]m_axi4_arlen;
+  output m_axi4_arlock;
+  output [2:0]m_axi4_arprot;
+  output [3:0]m_axi4_arqos;
+  input m_axi4_arready;
+  output [2:0]m_axi4_arsize;
+  output [15:0]m_axi4_aruser;
+  output m_axi4_arvalid;
+  output [39:0]m_axi4_awaddr;
+  output [1:0]m_axi4_awburst;
+  output [3:0]m_axi4_awcache;
+  output [15:0]m_axi4_awid;
+  output [7:0]m_axi4_awlen;
+  output m_axi4_awlock;
+  output [2:0]m_axi4_awprot;
+  output [3:0]m_axi4_awqos;
+  input m_axi4_awready;
+  output [2:0]m_axi4_awsize;
+  output [15:0]m_axi4_awuser;
+  output m_axi4_awvalid;
+  input [15:0]m_axi4_bid;
+  output m_axi4_bready;
+  input [1:0]m_axi4_bresp;
+  input m_axi4_bvalid;
+  input [31:0]m_axi4_rdata;
+  input [15:0]m_axi4_rid;
+  input m_axi4_rlast;
+  output m_axi4_rready;
+  input [1:0]m_axi4_rresp;
+  input m_axi4_rvalid;
+  output [31:0]m_axi4_wdata;
+  output m_axi4_wlast;
+  input m_axi4_wready;
+  output [3:0]m_axi4_wstrb;
+  output m_axi4_wvalid;
+  output m_axi4l_aclk;
+  output [39:0]m_axi4l_araddr;
+  output [0:0]m_axi4l_aresetn;
+  output [2:0]m_axi4l_arprot;
+  input m_axi4l_arready;
+  output m_axi4l_arvalid;
+  output [39:0]m_axi4l_awaddr;
+  output [2:0]m_axi4l_awprot;
+  input m_axi4l_awready;
+  output m_axi4l_awvalid;
+  output m_axi4l_bready;
+  input [1:0]m_axi4l_bresp;
+  input m_axi4l_bvalid;
+  input [63:0]m_axi4l_rdata;
+  output m_axi4l_rready;
+  input [1:0]m_axi4l_rresp;
+  input m_axi4l_rvalid;
+  output [63:0]m_axi4l_wdata;
+  input m_axi4l_wready;
+  output [7:0]m_axi4l_wstrb;
+  output m_axi4l_wvalid;
+  output out_clk100;
+  output out_clk50;
+  output out_clk50_90;
+  output [0:0]out_reset;
+
+
+endmodule
