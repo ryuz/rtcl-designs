8b50702c56f1b32d98120c9bb408573fa1e58382
Ryuji Fuchikami <ryuz@rtc-lab.com>
2026-06-19 23:57:36 +0900

    Refactor tb_main module: change logic to wire for FT601 signals and implement jelly3_stream_fifo integration

--- projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_fifo_sample/sim/tb_top/tb_main.sv
+++ projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_fifo_sample/sim/tb_top/tb_main.sv
@@ -13,17 +13,17 @@ module tb_main
     //  DUT
     // -------------------------
 
-    logic           ft601_reset_n   ;
-    logic           ft601_wakeup_n  ;
+    wire            ft601_reset_n   ;
+    wire            ft601_wakeup_n  ;
     logic           ft601_rxf_n     ;
     logic           ft601_txe_n     ;
     logic           ft601_siwu_n    ;
     logic           ft601_wr_n      ;
     logic           ft601_rd_n      ;
     logic           ft601_oe_n      ;
-    logic   [3:0]   ft601_be        ;
-    logic   [31:0]  ft601_data      ;
-    logic   [1:0]   ft601_gpio      ;
+    wire    [3:0]   ft601_be        ;
+    wire    [31:0]  ft601_data      ;
+    wire    [1:0]   ft601_gpio      ;
     logic   [1:0]   push_sw         ;
     logic   [1:0]   dip_sw          ;
     logic   [3:0]   led             ;
@@ -66,20 +66,58 @@ module tb_main
     //  Simulation
     // -------------------------
 
+    logic   [31:0]  cmd_data    ;
+    logic           cmd_valid   ;
+    logic           cmd_ready   ;
+
+    logic   [31:0]  rx_data     ;
+    logic           rx_valid    ;
+    logic           rx_ready    ;
+
+    jelly3_stream_fifo
+            #(
+                .ASYNC          (0              ),
+                .PTR_BITS       (10             ),
+                .DATA_BITS      (32             )
+            )
+        u_stream_fifo
+            (
+                .s_reset        (~ft601_reset_n ),
+                .s_clk          (ft601_clk      ),
+                .s_cke          (1'b1           ),
+                .s_data         (cmd_data       ),
+                .s_valid        (cmd_valid      ),
+                .s_ready        (cmd_ready      ),
+                .s_free_size    (               ),
+                
+                .m_reset        (~ft601_reset_n ),
+                .m_clk          (ft601_clk      ),
+                .m_cke          (1'b1           ),
+                .m_data         (rx_data        ),
+                .m_valid        (rx_valid       ),
+                .m_ready        (rx_ready       ),
+                .m_data_size    (               )
+        );
+
+
     assign push_sw[0] = reset;
     assign push_sw[1] = 1'b0;
 
-    logic [31:0] rx_fifo [$];
-
     initial begin
-        // write
-        rx_fifo.push_back(32'h0008_f0_02);
-        rx_fifo.push_back(32'h0000_0100);
-        rx_fifo.push_back(32'h1234_5678);
+        cmd_valid = 1'b0;
 
-        // read
-        rx_fifo.push_back(32'h0004_00_03);
-        rx_fifo.push_back(32'h0000_0000);
+        #10000;
+        $display("write");
+        @(negedge ft601_clk); cmd_valid = 1'b1; cmd_data = 32'h0008_f0_02;
+        @(negedge ft601_clk); cmd_valid = 1'b1; cmd_data = 32'h0000_0100;
+        @(negedge ft601_clk); cmd_valid = 1'b1; cmd_data = 32'h1234_5678;
+        @(negedge ft601_clk); cmd_valid = 1'b0;
+
+        #2000;
+        $display("read");
+        @(negedge ft601_clk); cmd_valid = 1'b1; cmd_data = 32'h0004_00_03;
+        @(negedge ft601_clk); cmd_valid = 1'b1; cmd_data = 32'h0000_0000;
+        @(negedge ft601_clk); cmd_valid = 1'b0;
     end
 
 
@@ -97,33 +135,12 @@ module tb_main
         dly_ft601_data <= ft601_data    ;
     end
 
-    // 疑似送受信
-    int     rd_data_count = '0;
-    int     rd_data;
-    int     wr_data_count = '0;
-
-    always_ff @(negedge ft601_clk) begin
-        if ( !ft601_reset_n ) begin
-            rd_data_count <= '0;
-            rd_data       <= '1;
-        end
-        else begin
-            // read
-            if ( ~dly_ft601_rd_n && rx_fifo.size() > 0 ) begin
-//              rd_data <= rx_fifo[0];
-                rx_fifo.pop_front();
-            end
-
-            // write
-            if ( ~ft601_txe_n && ~dly_ft601_wr_n ) begin
-            end
-        end
-    end
-    
-    assign ft601_rxf_n = ~(rx_fifo.size() > 0);
+  
+    assign ft601_rxf_n = ~rx_valid;
     assign ft601_txe_n = 1'b0;
-    assign ft601_data  = ~dly_ft601_oe_n ? rx_fifo[0] : 'z;
+    assign ft601_data  = ~dly_ft601_oe_n ? rx_data : 'z;
     assign ft601_be    = ~dly_ft601_oe_n ? '1 : 'z;
+    assign rx_ready    = ~dly_ft601_rd_n;
 
     // logging
     int fp_tx = 0;
