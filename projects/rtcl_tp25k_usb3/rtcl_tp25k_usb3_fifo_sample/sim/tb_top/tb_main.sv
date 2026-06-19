c73b981a4b3b12534c2fb3cf2ce0691885892dc5
Ryuji Fuchikami <ryuz@rtc-lab.com>
2026-06-19 19:43:02 +0900

    Remove commented-out write operations from rx_fifo initialization in tb_main

--- projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_fifo_sample/sim/tb_top/tb_main.sv
+++ projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_fifo_sample/sim/tb_top/tb_main.sv
@@ -72,12 +72,10 @@ module tb_main
     logic [31:0] rx_fifo [$];
 
     initial begin
-        /*
         // write
         rx_fifo.push_back(32'h0008_f0_02);
         rx_fifo.push_back(32'h0000_0100);
         rx_fifo.push_back(32'h1234_5678);
-        */
 
         // read
         rx_fifo.push_back(32'h0004_00_03);
