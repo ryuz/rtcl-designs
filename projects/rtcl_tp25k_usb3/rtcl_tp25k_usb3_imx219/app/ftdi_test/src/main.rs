82d31904e8d36b73a8a46128c5e65194e95d2ea2
Ryuji Fuchikami <r-fuchikami@rtc-lab.com>
2026-06-21 11:03:03 +0900

    WIP

---
+++ projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_imx219/app/ftdi_test/src/main.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+use libftd3xx_ffi::*;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Hello, world!");
+
+    
+    let mut num_devs : DWORD = 0;
+    let status = unsafe {FT_CreateDeviceInfoList(&mut num_devs)};
+    println!("Number of devices: {} (status = {})", num_devs, status);
+
+    let mut table_len: DWORD = 0;
+    let mut devices: Vec<FT_DEVICE_LIST_INFO_NODE> = Vec::with_capacity(num_devs as usize);
+    let status = unsafe { FT_GetDeviceInfoList(devices.as_mut_ptr(), std::ptr::addr_of_mut!(table_len))};
+    if status != 0 {
+        eprintln!("Failed to get device info list (status = {})", ret);
+        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Failed to get device info list")));
+    }
+
+    unsafe { devices.set_len(table_len as usize) };
+    println!("{:?} (status = {})", devices, ret);
+    println!("Table length: {}", table_len);
+    println!("Devices: {:?}", devices);
+
+    Ok(())
+}
