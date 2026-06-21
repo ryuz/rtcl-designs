2e44460c7bc16e80e1673b5155e1e788c17ea9a2
Ryuji Fuchikami <r-fuchikami@rtc-lab.com>
2026-06-21 21:29:26 +0900

    Refactor and enhance D3xx communication: update Cargo.toml and Cargo.lock for new dependencies; improve main.rs with USB and I2C handling; streamline rtcl_d3xx.rs for packet processing and command handling.

--- projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_imx219/app/rust/src/main.rs
+++ projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_imx219/app/rust/src/main.rs
@@ -11,198 +11,7 @@ use jelly_lib::imx219_sensor_driver::Imx219SensorDriver;
 
 use d3xx::{list_devices, Device, Pipe};
 
-mod rtcl_d3xx;
-use rtcl_d3xx::RtclD3xx;
 
-struct RtclD3xxAxi4lBus {
-    d3xx: Arc<Mutex<RtclD3xx>>,
-}
-
-impl RtclD3xxAxi4lBus {
-    fn new(d3xx: Arc<Mutex<RtclD3xx>>) -> Self {
-        Self { d3xx }
-    }
-
-    fn write_axi4l(&self, addr: u32, data: u32, strb: u8) -> Result<()> {
-        if let Ok(mut d3xx_guard) = self.d3xx.lock() {
-            d3xx_guard.write_axi4l(addr, data, strb)
-        } else {
-            Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to lock RtclD3xx"))
-        }
-    }
-
-    fn read_axi4l(&self, addr: u32) -> Result<u32> {
-        if let Ok(mut d3xx_guard) = self.d3xx.lock() {
-            d3xx_guard.read_axi4l(addr)
-        } else {
-            Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to lock RtclD3xx"))
-        }
-    }
-}
-
-impl Bus<u32, u32, u8> for RtclD3xxAxi4lBus {
-    type Error = std::io::Error;
-
-    fn write(&mut self, addr: u32, data: u32, strb: u8) -> Result<()> {
-        self.write_axi4l(addr, data, strb)?;
-        Ok(())
-    }
-
-    fn read(&mut self, addr: u32) -> Result<u32> {
-        let v = self.read_axi4l(addr)?;
-        Ok(v)
-    }
-}
-
-type UsbAccessor = SharedBusAccessor<RtclD3xxAxi4lBus, u32, u32, u8, LittleEndian>;
-
-const REGADR_SYSCTL_CONTROL0 : usize = 0x10;
-const REGADR_SYSCTL_CONTROL1 : usize = 0x11;
-const REGADR_SYSCTL_CONTROL2 : usize = 0x12;
-const REGADR_SYSCTL_CONTROL3 : usize = 0x13;
-
-
-fn main() {
-    println!("FT601 AXI4-Lite access test");
-
-    let all_devices = list_devices().expect("failed to list devices");
-    assert!(!all_devices.is_empty(), "No FT601 device found");
-    println!("Found {} devices", all_devices.len());
-
-    let device = all_devices[0].open().expect("failed to open device");
-    let device: Arc<Mutex<Device>> = Arc::new(Mutex::new(device));
-
-//  device.pipe(Pipe::In0).set_stream_size(Some(4*1024*1024)).expect("failed to set stream size");
-//  device.pipe(Pipe::In0).set_stream_size(None).expect("failed to set stream size");
-
-    let mut d3xx = RtclD3xx::new(device);
-    let id = d3xx.read_axi4l(0x00 * 4).expect("read_axi4l(id) failed");
-    println!("id : {id:04x}");
-
-    let d3xx_arc = Arc::new(Mutex::new(d3xx));
-    let axi4l_bus = RtclD3xxAxi4lBus::new(d3xx_arc.clone());
-    let usb_accessor = SharedBusAccessor::<RtclD3xxAxi4lBus, u32, u32, u8, LittleEndian>::new(axi4l_bus);
-    let ctl_acc = usb_accessor.subclone(0x0000_0000, 0x1000);
-    let i2c_acc = usb_accessor.subclone(0x0001_0000, 0x1000);
-
-    unsafe {
-        let id = ctl_acc
-            .try_read_reg_u32(0)
-            .expect("read_axi4l(scratch before) failed");
-        println!("id : {id:04x}");
-
-        let scratch = ctl_acc
-            .try_read_reg_u32(REGADR_SYSCTL_CONTROL3)
-            .expect("read_axi4l(scratch before) failed");
-        println!("scratch : {scratch:04x}");
-
-        ctl_acc
-            .try_write_reg_u32(REGADR_SYSCTL_CONTROL3, 0x1234)
-            .expect("read_axi4l(scratch before) failed");
-        println!("scratch : {scratch:04x}");
-
-        let scratch = ctl_acc
-            .try_read_reg_u32(REGADR_SYSCTL_CONTROL3)
-            .expect("read_axi4l(scratch before) failed");
-        println!("scratch : {scratch:04x}");
-
-    }
-
-    // カメラOFF
-    unsafe {
-        ctl_acc.write_reg_u32(REGADR_SYSCTL_CONTROL0, 0);
-        std::thread::sleep(Duration::from_millis(100));
-        ctl_acc.write_reg_u32(REGADR_SYSCTL_CONTROL1, 0);
-        std::thread::sleep(Duration::from_millis(100));
-    }
-
-    // カメラ電源ON
-    unsafe {
-        ctl_acc.write_reg_u32(REGADR_SYSCTL_CONTROL0, 1);
-        std::thread::sleep(Duration::from_millis(100));
-        ctl_acc.write_reg_u32(REGADR_SYSCTL_CONTROL1, 1);
-        std::thread::sleep(Duration::from_millis(100));
-    }
-
-    const IMX219_DEVADR: u8 =     0x10;    // 7bit address
-    let i2c = JellyI2c::<UsbAccessor>::new(i2c_acc, None);
-    let mut model_id: [u8; 2] = [0u8; 2];
-    i2c.write(IMX219_DEVADR, &[0x00, 0x00]);
-    i2c.read(IMX219_DEVADR, &mut model_id);
-    println!("model_id: 0x{:02x}{:02x}", model_id[0], model_id[1]);
-    let i2c = JellyI2cDevice::<IMX219_DEVADR, UsbAccessor>::new(i2c);
-
-    let mut imx219 = Imx219SensorDriver::new(i2c);
-    println!("sensor model ID:{:04x}", imx219.get_model_id().unwrap());
-
-    imx219.reset().unwrap();
-    std::thread::sleep(Duration::from_millis(100));
-
-    // camera 設定
-    let pixel_clock: f64 = 91000000.0;
-    let binning =  false;
-    let width: i32 = 256;
-    let height: i32 = 256;
-//  let width: i32 = 256;
-//  let height: i32 = 256;
-//    let frame_rate: i32 = 30;
-//    let exposure: i32 = 33;
-//    let a_gain: i32 = args.a_gain;
-//    let d_gain: i32 = args.d_gain;
-    let aoi_x: i32 = -1;
-    let aoi_y: i32 = -1;
-    let flip_h: bool = false;
-    let flip_v: bool = false;
-    imx219.set_pixel_clock(pixel_clock).unwrap();
-    imx219.set_aoi(width, height, aoi_x, aoi_y, binning, binning).unwrap();
-    imx219.set_frame_rate(30.0).unwrap();
-    imx219.set_exposure_time(33.0).unwrap();
-
-    imx219.set_gain(3.0).unwrap();
-    imx219.set_digital_gain(1.0).unwrap();
-
-    print!("wait key : start camera...");
-    std::io::stdout().flush().unwrap();
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();   
-
-    imx219.start().unwrap();
-
-
-    let mut img_list = Vec::<Vec<u8>>::new();
-    if let Ok(mut d3xx_guard) = d3xx_arc.lock() {
-        for _ in 0..256+1 {
-            let pkt = d3xx_guard.recv_axi4s().unwrap();
-            img_list.push(pkt);
-//          println!("Received AXI4S packet: {:?}", pkt);
-        }
-    }
-
-    for (i, img) in img_list.iter().enumerate() {
-        // ファイルに保存
-        let filename = format!("rec/image_{:03}.bin", i);
-        std::fs::write(&filename, img).expect("Failed to write image file");
-    }
-
-    /*
-    if let Ok(mut d3xx_guard) = d3xx_arc.lock() {
-        loop {
-            let pkt = d3xx_guard.recv_axi4s().unwrap();
-        }
-    }
-    */
-
-    return;
-    // キー入力待ち
-    print!("進むには Enter キーを押してください...");
-    std::io::stdout().flush().unwrap();
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();   
-}
-
-
-
-/*
 struct Axi4LiteD3xx {
     device: Device,
 }
@@ -236,6 +45,7 @@ impl Axi4LiteD3xx {
             ((data >> 24) & 0xff) as u8,
         ];
 
+//        println!("write_axi4l: packet{:?}", packet);
         self.device.pipe(Pipe::Out0).write_all(&packet)?;
         thread::sleep(Duration::from_millis(20));
 
@@ -256,10 +66,13 @@ impl Axi4LiteD3xx {
             ((addr >> 24) & 0xff) as u8,
         ];
 
+//        println!("read_axi4l: packet {:?}", packet);
         self.device.pipe(Pipe::Out0).write_all(&packet)?;
 
         let mut resp = [0u8; 8];
         self.device.pipe(Pipe::In0).read_exact(&mut resp)?;
+//        println!("read_axi4l: resp = {:?}", &mut resp);
+
         Ok(u32::from_le_bytes([resp[4], resp[5], resp[6], resp[7]]))
     }
 
@@ -331,7 +144,6 @@ fn main() {
         .expect("read_axi4l(scratch after) failed");
     println!("scratch : {scratch:04x}");
 
-
 //  let accessor: SharedBusAccessor<Axi4LiteD3xx, u32, u32, u8, LittleEndian> = SharedBusAccessor::new(axi);
     let accessor = UsbAccessor::new(axi);
     unsafe {
@@ -362,6 +174,8 @@ fn main() {
     let mut imx219 = Imx219SensorDriver::new(i2c);
     println!("sensor model ID:{:04x}", imx219.get_model_id().unwrap());
 
+//  return;
+
     // camera 設定
     let pixel_clock: f64 = 91000000.0;
     let binning =  true;
@@ -388,4 +202,3 @@ fn main() {
     std::io::stdin().read_line(&mut input).unwrap();
 }
 
-*/
--- projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_imx219/app/rust/src/rtcl_d3xx.rs
+++ projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_imx219/app/rust/src/rtcl_d3xx.rs
@@ -5,111 +5,30 @@ use std::time::Duration;
 
 use d3xx::{Device, Pipe};
 
-const OPCODE_AXI4L_WRITE: u8 = 0x02;
-const OPCODE_AXI4L_READ: u8 = 0x03;
-const OPCODE_AXI4S_TRANS: u8 = 0x10;
+const OPCODE_AXI4L: u8 = 0x03;
+const OPCODE_AXI4S: u8 = 0x10;
 
 #[derive(Debug, Clone)]
 pub struct RtclPacket {
     pub opcode: u8,
-    pub operand: u8,
+    pub opland: u8,
     pub payload: Vec<u8>,
 }
-
-use std::sync::mpsc;
 
 type PacketFifo = Arc<(Mutex<VecDeque<RtclPacket>>, Condvar)>;
 
 pub struct RtclD3xx {
-    device: Arc<Mutex<Device>>,
-//  axi4s_buf: Vec<u8>,
-    rx_command: mpsc::Receiver<RtclPacket>,
-    rx_stream: mpsc::Receiver<RtclPacket>,
+    device: Device,
+    axi4s_buf: Vec<u8>,
+    axi4s_packets: Vec<Vec<u8>>,
 }
 
 impl RtclD3xx {
-    pub fn new(device: Arc<Mutex<Device>>) -> Self {
-        let (tx_command, rx_command) = mpsc::channel::<RtclPacket>();
-        let (tx_stream, rx_stream) = mpsc::channel::<RtclPacket>();
-        let device_rx = Arc::clone(&device);
-
-        std::thread::spawn(move || {
-//          let all_devices = d3xx::list_devices().expect("failed to list devices");
-//          let device = all_devices[0].open().expect("failed to open device");
-            let mut buf = vec![0u8; 4];
-            let mut header = true;
-            let mut packet = RtclPacket { opcode : 0, operand: 0, payload: Vec::new() };
-            let mut pkt_len = 0;
-            loop {
-                // 受信
-                println!("start recv");
-                let mut offset = 0;
-                let ret = {
-                    let device_guard = device_rx.lock().unwrap();
-                    let mut recv_pipe = device_guard.pipe(Pipe::In0).clone();
-                    recv_pipe.set_timeout(1).expect("failed to set timeout");
-                    recv_pipe.read(&mut buf)
-                };
-                println!("end recv");
-
-                // パケット分析
-                if let Ok(mut size) = ret {
-                    assert!(size % 4 == 0);  // 32bit単位でしか通信しない
-                    while size > 0 {
-                        if header {
-                            // ヘッダを処理
-                            packet.opcode = buf[offset];
-                            packet.operand = buf[offset + 1];
-                            pkt_len = u16::from_le_bytes([buf[offset + 2], buf[offset + 3]]) as usize;
-                            offset += 4;
-                            size -= 4;
-                            if pkt_len == 0 {
-                                // ショートコマンドなら即処理
-                                if packet.opcode == OPCODE_AXI4S_TRANS {
-                                    tx_stream.send(packet.clone()).unwrap();
-                                }
-                                else {
-                                    tx_command.send(packet.clone()).unwrap();
-                                }
-                                packet.payload.clear();
-                                println!("recv_packet: opcode = 0x{:02x}, operand = 0x{:02x}, length = 0x{:04x}", packet.opcode, packet.operand, pkt_len);
-                            }
-                            else {
-                                header = false;
-                            }
-                        }
-                        else {
-                            // 後続ペイロードの処理
-                            if size >= pkt_len {
-                                packet.payload.extend_from_slice(buf[offset..offset + pkt_len].as_ref());
-                                offset += pkt_len;
-                                size -= pkt_len;
-                                header = true;
-                                if packet.opcode == OPCODE_AXI4S_TRANS {
-                                    tx_stream.send(packet.clone()).unwrap();
-                                }
-                                else {
-                                    tx_command.send(packet.clone()).unwrap();
-                                }
-                                packet.payload.clear();
-                                println!("recv_packet: opcode = 0x{:02x}, operand = 0x{:02x}, length = 0x{:04x}", packet.opcode, packet.operand, pkt_len);
-                            }
-                            else {
-                                packet.payload.extend_from_slice(buf[offset..offset + size].as_ref());
-                                pkt_len -= size;
-                                size = 0;
-                            }
-                        }
-                    }
-                }
-            }
-        });
-
+    pub fn new(device: Device) -> Self {
         Self {
             device,
-//          axi4s_buf: Vec::<u8>::new(),
-            rx_command: rx_command,
-            rx_stream: rx_stream,
+            axi4s_buf: Vec::<u8>::new(),
+            axi4s_packets: Vec::<Vec<u8>>::new(),
         }
     }
 
@@ -121,49 +40,56 @@ impl RtclD3xx {
         packet.push(operand);
         packet.extend_from_slice(&length.to_le_bytes());
         packet.extend_from_slice(payload);
-//      println!("send_packet: packet = {:?}", packet);
-        {
-            let device_guard = self.device.lock().unwrap();
-            let mut send_pipe = device_guard.pipe(Pipe::Out0).clone();
-            send_pipe.write_all(&packet)?;
-        }
+        println!("send_packet: packet = {:?}", packet);
+        self.device.pipe(Pipe::Out0).write_all(&packet)?;
         Ok(())
     }
 
     // 受信パケットを1つ読み込む
-    /*
     fn recv_packet(&mut self) -> Result<(u8, u8, Vec<u8>)> {
         let mut header = [0u8; 4];
         self.device.pipe(Pipe::In0).read_exact(&mut header)?;
-//      println!("recv_packet: header = {:?}", header);
+        println!("recv_packet: header = {:?}", header);
         let opcode = header[0];
         let operand = header[1];
-        let length = u16::from_le_bytes([header[2], header[3]]) as usize;// & 0xff;
-        println!("recv_packet: opcode = 0x{:02x}, operand = 0x{:02x}, length = 0x{:04x}", opcode, operand, length);
+        let length = u16::from_le_bytes([header[2], header[3]]) as usize;
 
         let mut payload = vec![0u8; length as usize];
-        if length > 0 {
-            self.device.pipe(Pipe::In0).read_exact(&mut payload)?;
-//          println!("recv_packet: payload = {:?}", payload);
-        }
+        self.device.pipe(Pipe::In0).read_exact(&mut payload)?;
+        println!("recv_packet: payload = {:?}", payload);
         Ok((opcode, operand, payload))
     }
-    */
 
     fn recv_command(&mut self) -> Result<(u8, u8, Vec<u8>)> {
-        self.rx_command.recv().map(|packet| (packet.opcode, packet.operand, packet.payload)).map_err(|e| Error::new(ErrorKind::Other, e))
+        loop {
+            let (opcode, operand, payload) = self.recv_packet()?;
+            if opcode == OPCODE_AXI4S {
+                self.axi4s_buf.extend_from_slice(&payload);
+                if operand & 0x80 != 0 {
+                    self.axi4s_packets.push(self.axi4s_buf.clone());
+                    self.axi4s_buf.clear();
+                }
+            }
+            else {
+                return Ok((opcode, operand, payload));
+            }
+        }
     }
 
     pub fn recv_axi4s(&mut self) -> Result<Vec<u8>> {
-        let mut payload = Vec::<u8>::new();
-        loop {
-            let packet = self.rx_stream.recv().map_err(|e| Error::new(ErrorKind::Other, e))?;
-            assert!(packet.opcode == OPCODE_AXI4S_TRANS, "Expected AXI4S packet");
-            payload.extend_from_slice(&packet.payload);
-            if packet.operand & 0x80 != 0 {
-                return Ok(payload);
+        if self.axi4s_buf.is_empty() {
+            loop {
+                let (opcode, operand, payload) = self.recv_packet()?;
+                assert!(opcode == OPCODE_AXI4S, "Expected AXI4S packet");
+                self.axi4s_buf.extend_from_slice(&payload);
+                if operand & 0x80 != 0 {
+                    self.axi4s_packets.push(self.axi4s_buf.clone());
+                    self.axi4s_buf.clear();
+                    break;
+                }
             }
         }
+        Ok(self.axi4s_packets.remove(0))
     }
 
 
@@ -172,13 +98,13 @@ impl RtclD3xx {
         let mut payload = Vec::<u8>::with_capacity(8);
         payload.extend_from_slice(addr.to_le_bytes().as_ref());
         payload.extend_from_slice(data.to_le_bytes().as_ref());
-        self.send_packet(OPCODE_AXI4L_WRITE, strb << 4, &payload)?;
+        self.send_packet(OPCODE_AXI4L, strb, &payload)?;
 
         // 応答受信
         let (opcode, operand, payload) = self.recv_command()?;
-        assert!(opcode == OPCODE_AXI4L_WRITE, "Expected AXI4L_WRITE response");
+        assert!(opcode == OPCODE_AXI4L, "Expected AXI4L response");
         assert!(operand == 0, "Expected AXI4L response");
-        assert!(payload.len() == 0, "Expected 04 bytes in AXI4L_WRITE response");
+        assert!(payload.len() == 4, "Expected 4 bytes in AXI4L response");
         Ok(())
     }
 
@@ -186,13 +112,13 @@ impl RtclD3xx {
         // コマンド送信
         let mut payload = Vec::<u8>::with_capacity(4);
         payload.extend_from_slice(addr.to_le_bytes().as_ref());
-        self.send_packet(OPCODE_AXI4L_READ, 0, &payload)?;
+        self.send_packet(OPCODE_AXI4L, 0, &payload)?;
 
         // 応答受信
         let (opcode, operand, payload) = self.recv_command()?;
-        assert!(opcode == OPCODE_AXI4L_READ, "Expected AXI4L_READ response");
+        assert!(opcode == OPCODE_AXI4L, "Expected AXI4L response");
         assert!(operand == 0, "Expected AXI4L response");
-        assert!(payload.len() == 4, "Expected 4 bytes in AXI4L_READ response");
+        assert!(payload.len() == 4, "Expected 4 bytes in AXI4L response");
         let data = u32::from_le_bytes(payload[0..4].try_into().unwrap());
         Ok(data)
     }
