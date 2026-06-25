a387c6d1e96c72ef3636c067f11db56db9d1ad0c
Ryuji Fuchikami <ryuz@rtc-lab.com>
2026-06-25 16:58:47 +0900

    Update tb_main and Makefile for rtcl_tp25k_usb3_axi4l integration: add MIPI signals and modify source file paths

--- projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_axi4l/sim/tb_top/tb_main.sv
+++ projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_axi4l/sim/tb_top/tb_main.sv
@@ -24,13 +24,19 @@ module tb_main
     wire    [3:0]   ft601_be        ;
     wire    [31:0]  ft601_data      ;
     wire    [1:0]   ft601_gpio      ;
+
+    wire            mipi_scl        ;
+    wire            mipi_sda        ;
+    wire    [1:0]   mipi_gpio       ;
+    logic           mipi_pwr_en_n   ;
+
     logic   [1:0]   push_sw         ;
     logic   [1:0]   dip_sw          ;
     logic   [3:0]   led             ;
     logic   [7:0]   pmod            ;
 
-    rtcl_tp25k_usb3_fifo_multi_channel
-        u_rtcl_tp25k_usb3_fifo_multi_channel
+    rtcl_tp25k_usb3_axi4l
+        u_rtcl_tp25k_usb3_axi4l
             (
                 .in_clk50           (clk            ),
                 .ft601_reset_n      (ft601_reset_n  ),
@@ -45,6 +51,10 @@ module tb_main
                 .ft601_be           (ft601_be       ),
                 .ft601_data         (ft601_data     ),
                 .ft601_gpio         (ft601_gpio     ),
+                .mipi_scl           (mipi_scl       ),
+                .mipi_sda           (mipi_sda       ),
+                .mipi_gpio          (mipi_gpio      ),
+                .mipi_pwr_en_n      (mipi_pwr_en_n  ),
                 .push_sw            (push_sw        ),
                 .dip_sw             (dip_sw         ),
                 .led                (led            ),
@@ -99,7 +109,7 @@ module tb_main
             @(negedge ft601_clk);
         end
 
-        // 受信
+        // Read Command
         ft601_rxf_n  = 1'b1;
         ft601_data_t = 32'hffff_00ff;
         ft601_data_o = 32'h0000_ef00;
@@ -111,14 +121,19 @@ module tb_main
         @(negedge ft601_clk);
         @(negedge ft601_clk);
 
-        for ( int i = 1; i < 11; i++ ) begin
-            ft601_rxf_n  = 1'b0;
-            ft601_be_t   = 4'h0         ;
-            ft601_be_o   = 4'h1         ;
-            ft601_data_t = 32'h0000_0000;
-            ft601_data_o = 32'(i);
-            @(negedge ft601_clk);
-        end
+        ft601_rxf_n  = 1'b0;
+        ft601_be_t   = 4'h0         ;
+        ft601_be_o   = 4'h1         ;
+        ft601_data_t = 32'h0000_0000;
+        ft601_data_o = 32'h0004_0003;   // read command
+        @(negedge ft601_clk);
+
+        ft601_rxf_n  = 1'b0;
+        ft601_be_t   = 4'h0         ;
+        ft601_be_o   = 4'h1         ;
+        ft601_data_t = 32'h0000_0000;
+        ft601_data_o = 32'h0000_0000;   // read address
+        @(negedge ft601_clk);
 
         ft601_rxf_n  = 1'b1;
         ft601_be_t   = 4'hf         ;
@@ -126,7 +141,6 @@ module tb_main
         ft601_data_t = 32'hffff_00ff;
         ft601_data_o = 32'h0000_ff00;
         @(negedge ft601_clk);
-
 
         @(negedge ft601_clk);
         @(negedge ft601_clk);
@@ -134,7 +148,7 @@ module tb_main
             @(negedge ft601_clk);
         end
 
-        // 送信1
+        // Ack
         ft601_rxf_n  = 1'b1;
         ft601_data_t = 32'hffff_00ff;
         ft601_data_o = 32'h0000_fe00;
@@ -158,44 +172,6 @@ module tb_main
         ft601_be_o   = 4'h3         ;
         ft601_data_t = 32'hffff_00ff;
         ft601_data_o = 32'h0000_ff00;
-
-        // interval
-        @(negedge ft601_clk);
-        @(negedge ft601_clk);
-        @(negedge ft601_clk);
-        @(negedge ft601_clk);
-
-        // 送信2
-        ft601_rxf_n  = 1'b1;
-        ft601_data_t = 32'hffff_00ff;
-        ft601_data_o = 32'h0000_fe00;
-        @(negedge ft601_clk);
-
-        while ( dly_ft601_wr_n != 1'b0 ) begin
-            @(negedge ft601_clk);
-        end
-        ft601_be_t   = 4'hf         ;
-        ft601_data_t = 32'hffff_ffff;
-        @(negedge ft601_clk);
-        ft601_rxf_n  = 1'b0;
-
-        @(negedge ft601_clk);
-        @(negedge ft601_clk);
-        @(negedge ft601_clk);
-        @(negedge ft601_clk);
-        @(negedge ft601_clk);
-        @(negedge ft601_clk);
-        @(negedge ft601_clk);
-        @(negedge ft601_clk);
-        @(negedge ft601_clk);
-        ft601_rxf_n  = 1'b1;
-        @(negedge ft601_clk);
-        ft601_rxf_n  = 1'b1;
-        ft601_be_t   = 4'hf         ;
-        ft601_be_o   = 4'h3         ;
-        ft601_data_t = 32'hffff_00ff;
-        ft601_data_o = 32'h0000_ff00;
-
 
         // end
         @(negedge ft601_clk);
@@ -207,33 +183,6 @@ module tb_main
         $finish;
     end
     
-
-
-    /*
-    // logging
-    int fp_tx = 0;
-    int fp_rx = 0;
-    initial begin
-        fp_tx = $fopen("tx_log.txt", "w");
-        fp_rx = $fopen("rx_log.txt", "w");
-    end
-
-    always_ff @(negedge ft601_clk) begin
-        if ( ft601_reset_n ) begin
-            if ( ~ft601_rxf_n && ~dly_ft601_rd_n && ~dly_ft601_oe_n ) begin
-                $fdisplay(fp_rx, "%h", ft601_data);
-            end
-        end
-    end
-
-    always_ff @(negedge ft601_clk) begin
-        if ( ft601_reset_n ) begin
-            if ( ~ft601_txe_n && ~dly_ft601_wr_n ) begin
-                $fdisplay(fp_tx, "%h", dly_ft601_data);
-            end
-        end
-    end
-    */
 
 endmodule
 
