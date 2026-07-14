ae48d22816e1c564ae3f8fa9811628b0aa9954b4
Ryuji Fuchikami <r-fuchikami@rtc-lab.com>
2026-07-14 13:30:56 +0900

    Update pmod assignments to use ft601_data_i signals for improved functionality

--- projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_morphology_filter/rtl/rtcl_tp25k_usb3_morphology_filter.sv
+++ projects/rtcl_tp25k_usb3/rtcl_tp25k_usb3_morphology_filter/rtl/rtcl_tp25k_usb3_morphology_filter.sv
@@ -436,8 +436,8 @@ module rtcl_tp25k_usb3_morphology_filter
     assign pmod[3] = axi4s_ft601_rx[1].tvalid;
     assign pmod[4] = axi4s_ft601_tx[1].tready;
     assign pmod[5] = axi4s_ft601_tx[1].tvalid;
-    assign pmod[6] = ft601_txe_n;
-    assign pmod[7] = ft601_data_i[9];
+    assign pmod[6] = ft601_data_i[9];
+    assign pmod[7] = ft601_data_i[13];
 
     /*
     assign pmod[0] = dphy_byte_ready;
